e5984b75fd1c3941e94c088dbe4d8087221a8d00
Rmartinez75 <rmartinez75@gmail.com>
2020-06-30 11:56:21 -0400

    MongoDb Comit

--- index.js
+++ index.js
@@ -3,27 +3,19 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   uuid = require('uuid');
 
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/flixOlogyDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const app = express();
 
 app.use(bodyParser.json());
-
-let movies = [
-  {
-    movieName: 'Vanilla Sky',
-  },
-  {
-    movieName: 'Sunshine',
-  },
-  {
-    movieName: 'Avengers: End Game',
-  },
-  {
-    movieName: 'Eternal Sunshine of the Spotless Mind',
-  },
-  {
-    movieName: 'The Life Aquatic with Steve Zissou',
-  },
-];
 
 app.use(morgan('common'));
 
@@ -33,9 +25,13 @@ app.get('/', (req, res) => {
   res.send('Welcome to flixOlogy!');
 });
 
-app.get('/movies', (req, res) => {
-  res.json(movies);
+app.get('/movies', function (req, res) {
+  Movies.find().then((Movies) => res.json(Movies));
 });
+
+// app.get('/movies', (req, res) => {
+//   res.json(movies);
+// });
 
 app.get('/movies/info', (req, res) => {
   res.send('Succesful GET request returning detailed information of movie');
@@ -52,18 +48,107 @@ app.get('/castCrew/director/:name', (req, res) => {
 });
 
 app.post('/users', (req, res) => {
-  res.send('Succesful POST request registering new user');
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
-app.put('/users/:name', (req, res) => {
-  res.send('Succesful PUT request updating existing users information');
+// app.post('/users', (req, res) => {
+//   res.send('Succesful POST request registering new user');
+// });
+
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-app.put('/users/:name/favorites', (req, res) => {
-  res.send(
-    'Succesful PUT request saving a specified movie into favorites list'
+app.get('/users/:username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
   );
 });
+// app.put('/users/:name', (req, res) => {
+//   res.send('Succesful PUT request updating existing users information');
+// });
+
+app.post('/users/:Username/Movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $push: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(er);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
+});
+
+// app.put('/users/:name/favorites', (req, res) => {
+//   res.send(
+//     'Succesful PUT request saving a specified movie into favorites list'
+//   );
+// });
 
 app.delete('/users/:name/favorites', (req, res) => {
   res.send(
