145e53c07037d09f3649b67a8376ace0ec634d84
Rmartinez75 <rmartinez75@gmail.com>
2020-07-13 05:22:53 -0400

    Second attempt at upload for testing app

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,12 +1,48 @@
 import React from 'react';
 import axios from 'axios';
 
-class MainView extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
+// class MainView extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {};
+//   }
+//   render() {
+//     return <div className='main-view'></div>;
+//   }
+// }
+
+export class MainView extends React.Component {
+  // One of the "hooks" available in a React Component
+  componentDidMount() {
+    axios
+      .get('https://flixology.herokuapp.com/movies')
+      .then((response) => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
+
   render() {
-    return <div className='main-view'></div>;
+    // If the state isn't initialized, this will throw on runtime
+    // before the data is initially loaded
+    const { movies } = this.state;
+
+    // Before the movies have been loaded
+    if (!movies) return <div className='main-view' />;
+
+    return (
+      <div className='main-view'>
+        {movies.map((movie) => (
+          <div className='movie-card' key={movie._id}>
+            {movie.Title}
+          </div>
+        ))}
+      </div>
+    );
   }
 }
